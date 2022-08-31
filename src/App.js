a88abbb1aa6a029d0b2dff0a284afb3b778cbd92
Andres Alba <tensinkusum@example.com>
2022-08-31 01:07:21 -0500

    ReactToolboxTable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import DataTable from 'react-data-table-component';
+import mock from './MOCK_DATA.json';
+
+const data = mock; //MOCK_DATA.json source: https://www.mockaroo.com/
+
+const columns = [
+  {
+    name: "first_name",
+    selector: row => row.first_name
+  },
+  {
+    name: "last_name",
+    selector: row => row.last_name
+  },
+  {
+    name: "email",
+    selector: row => row.email
+  },
+  {
+    name: "gender",
+    selector: row => row.gender
+  },
+  {
+    name: "ip_address",
+    selector: row => row.ip_address
+  }
+] 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Data Table Component</h1>
+      <DataTable
+        columns={columns}
+        data={data}
+      />
     </div>
   );
 }
